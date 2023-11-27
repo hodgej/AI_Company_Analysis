d497faff2ea2e59357bf3f590862b932dbc915de
Jack Hodge <hodgej@bentonvillek12.org>
2023-11-27 13:49:03 -0600

    initial

--- src/main.java
+++ src/main.java
@@ -1,5 +1,11 @@
 public class main {
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println("Home of the software which gets user input, scrapes the web, " +
+                           "and feeds that web data into a pretrained AI Model");
+
+        // Web scraping class -- format for holding that data (JSON?)
+        // AI Model
+        // GUI Output system
     }
 }
